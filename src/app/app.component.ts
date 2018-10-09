7e6122169ea9ee6cadb78ee8642c95e0cfae13e2
Kumaran MDL <mdlkumaran@gmail.com>
2018-10-09 06:30:14 -0700

    adding new feature files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'shopping-angular6';
+  loadedFeature = 'recipe';
+
+  onNavigate(feature: string) {
+    this.loadedFeature  = feature;
+  }
 }
